bf5c0618e48750ee82926c7975d6ed72ba9a7d48
D3rKuckuck <146887190+D3rKuckuck@users.noreply.github.com>
2024-11-07 20:12:46 +0300

    Лабораторная работа 3 (исправления)
    Учел ваши указания на ошибки. 
    Во втором задании использовал множества и нужный метод.
    В первом задании исправил название функции и использовал enumerate()
    В третьем всё сделал на кириллице и использовал динамический метод заполнения словаря.

---
+++ Лабораторная работа 3/task_3.py
@@ -0,0 +1,37 @@
+#6d0QpGT7hl8tQdzUoJqg9szh74Onck4kkU3p6hiX55mL9B/4cWZY05VBLpeyclwwYtEy3KpD/CmjI3+AhQohc3FqEZqWL0ywSptGN2zQWNp1Ne3BbLlWs6s9rjXLcu8jgVnwY04upmuFlTUzhBXa+xXgLP6p4FaXk6BOQXHuGG/ORQrfowj2OMjypvi4gCTZWdzplxRe77JvvPoVLQ3jVOWXPNOIgFivF+bSSKAH5CBqLiQtS0YGPT1A7pvo/f5FFPrNJIwkBMd/QOZLT9HewEdQJC92729WeQjj/CB++AJOf9dzfdiy42DmQIKLVi/LvqP+O9HfcFY8H1mQdAFwtDB/z8T+VOjRlW9hVpElnksg/Z1NRtPkSsPpcRougwdbc2DCqaM6DHCeoGWRN/MWC9wOFtQcMwAeCQXusy7uGwZwfbl1fEOAV+S71OO3TJkx0Hi6XMMYRGVDlE4lsCTOPEvS+U6d6YW0FV8KhFzifygp42PfOyG0t413BzON3D+WIcLXvHDOKnHssAo4E8cfIacRuDc3II+ObCAgLjcyFC6NWXnyH5DGVI0eAcPh9JmS8QW9Db/XmAcDh0WGff3TB6JjgqiZUzIkP7UtjXy7b9y+cOnL024yrEMUfX0bRBv0gcBCEQQSfkDyxKveasVAOT2svyINsgWYIPY6UAlT8d4vuBI7HRVzSmE5+wTK0iyyq3OVtzudU+FZ0PIdTre0bklrwSCFJUmz/Uo2ZDKpxkKn7DMOMUQDmXE10oCgqnlVS0UFwas0Ja2ysSZVeO6+K9qiWeD+99jjWQ4VEc/XIVBwQ5rZUDMxWOu+6Yp2FN9THL+zULfIrXbB8lp3xYoT0FyqZUEkckIq/ZpvhU4Dz5UEPR97U8fYGMxvX41UlgOUgvyBq7Y1mZ7l7dnU7wo8hWOg7r/uHuiiKTm2ntOdxDJraNt6SYSDtwRCVV07fcHWvr4WCQ/2iLe5zAWmcY+ucODtNJK49zrq9FKlHeS1EUgHnEuJeooXznuFjGS/DljX8Om/5FSal9hJYLsFqGKQcsK7A6Ni6LOAnvoZJXqsjGVnvqU8LDFn2EPz1TmdMuDIAlUdPp0cbuMyOHT3h6zGgiMMLHTGYN35/5AB6RScdHYe+fSecDorchsS6SEv6gr0DB9X0CFCmZ5VgZd/8N02pzkRg55VL/J2PvAQTWw576QJT0qohqAxw9cEAaOOO52JulpCXjwYO7vWchuiw4+mlmN0txYWfLOnvu45tHOBUeCp5evmKZoGJNvRSLzvezCUAZPTGkb4yilzw09HLT45TkRKNXdLH1KkMgBOpcOiyZ9FDiSgD5tTVXetLAqXMWGBAQuY1jNqVzIUDwKYaCtZQjTRojYUjc7H4st0A9MVFEHJdCjWhct+1+iFVOnkwj5T3JICOUs07eOUcO3NDlz1xqBwDfRt8H02cNdlMUS7zWjIhpXBmsEiAaa9t2u7IVxS4ZNXs9sA89HhEBDV2o2mxT4dRzmE8AiqITChF8F/CqC2n0pabCUa5RBamZA3UBoIjo0aorwBtf4/rCy7PrjA3ezL5i6cpoxc33+jqqVRF+z8M/l4HhTXQQNMdsYIYhtedaM/86t3i4ZVhQ9VF2NA4vjLQOSiLwVSVVr2Dk6sgDoHH3YgL0VcNvNjxzh7+8MislElgXOxB5aHEp9cTjeEaF8OZIGesetK9ybCTkaeMoU94x+d+2Kq5oueq4Vm3qGKdoTTZwRolCh27MmTzM+ybE4aNXXOcES/xROWUztvteR392OGK5DzJdBIOvdAko3zrQdpkZgyyX+XvQ3GNBUwHq2qISuFEWV2NacmI++8XLBMUIndHoeE1zny01hTW5jqTT1XiEPfnjaxkKvYmVP9qSwL9KtLmhMcM8NRAV2dCtKC7zTcr0rUymVrJp7hdRpc2EFLutBmvQ+RJ4tdmMjzCq31GdKe49TuH3V5fRK34FG9XsXPl39bURIegITyOvd/z+ByHRsHIrCjVSqog6LZX0EZj3xwaq6cGVJEA4KU3borRzQAzht7JmoiafAtV/DD2jV9HODWXsRyBBxZhBHsqrX9f9z9MKTUZYHOrsig2IclwSK0UGcJ34yJw1ZfH+ruKxdMN5kYOR81tN99WY/cAlZ13yzwAt7eotoL/0HjNluDWGX8mJgQgWnlvGiNRLV2NcV31p4ETOFfkK//2QU36jT8EAM0UHWVk0Oy2I4GN/2E6Pw6OgYOQPFUVXQq4mPpH5FIoUCA+6cVqb6Qezpg7+Sj3CT+DB6JWQB+QuviDJQNqD3MWkACbkMafNHFY5VYmEh59MG7OSCbZcfYPjynCQUh5bBUXnkP0JQtqtfBDz/A+/pw/lFxx3COx/GOBvlAx707jnP15Yac+g09c1RKrKKhwiZNbrF1aVmDqtc5PMesauMA==
+
+# Частотный анализ букв в тексте
+def count_letters(text):
+    text = text.lower()
+    counted = {}
+    for alpha in text:
+        if alpha.isalpha():
+            counter = 0
+            for char in text:
+                if char == alpha:
+                    counter += 1
+            counted.setdefault(alpha, counter)
+    return counted
+
+def calculate_frequency(frecuencies):
+    counter = sum(frecuencies.values())
+    for key in frecuencies:
+        frecuency = frecuencies[key] / counter
+        frecuency = round(frecuency, 2)
+        frecuencies[key] = frecuency
+    return frecuencies
+
+#My text
+text = "Умей прощать, и мощь твоя умножится. (Публий Сир)"
+
+# Вывод кол-ва
+print(text,'\n')
+print("Количество букв:")
+for key, value in count_letters(text).items():
+    print(f'{key}:', value)
+
+# Вывод частоты
+print("\nЧастота появления в тексте:")
+frecuencies = calculate_frequency(count_letters(text))
+for key, value in frecuencies.items():
+    print(f'{key}:', value)
